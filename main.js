1cfd6385779ae5757e180f02565069dfc0bd984a
Abed Ali <mdabedali15@gmail.com>
2023-10-27 13:26:22 +0600

    update hex code with rgb

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 const cngBtn = document.querySelector('#cng-btn')
-const displayColorCode = document.querySelector('.display-color-code')
+const rgbCode = document.querySelector('.rgb-code')
+const hexCode = document.querySelector('.hex-code')
 
 
 cngBtn.addEventListener('click', () => {
-    document.body.style.backgroundColor = colorRGB()
-    displayColorCode.innerHTML=colorRGB()
+    const color = decimalColorCode()
+    document.body.style.backgroundColor = colorHEX(color)
+    rgbCode.innerHTML = colorRGB(color)
+    hexCode.innerHTML=colorHEX(color)
 })
 
 
-
-//generate RGB color
-function colorRGB() {
+//generate decimal color code
+function decimalColorCode() {
     const red = Math.floor(Math.random() * 255)
     const green = Math.floor(Math.random() * 255)
     const blue = Math.floor(Math.random() * 255)
-    
+    return {
+        red,
+        green,
+        blue
+    }
+}
+
+
+//generate RGB color
+function colorRGB({red,green,blue}) {
     return `rgb(${red},${green},${blue})`
- }
+}
+ 
+
+//generate HEX color
+function colorHEX({red,green,blue}) {
+    const r = red.toString(16)
+    const g = green.toString(16)
+    const b = blue.toString(16)
+
+    return `#${r}${g}${b}`
+}
+
+
+
