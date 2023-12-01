17d0d0b7dc064cdea0d5339d31ce11b1133171b2
Abed Ali <mdabedali15@gmail.com>
2023-12-01 11:48:28 +0600

    UI update

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ import decimalColorCode from "./decimalColorCode.js"
 import colorRGB from "./colorRgb.js"
 import colorHEX from "./colorHex.js"
 
-const cngBtn = document.querySelector('#cng-btn')
+const cngBtn = document.querySelector('#change-btn')
+const colorDisplay = document.querySelector('.color-display')
 const rgbCode = document.querySelector('.rgb-code')
 const hexCode = document.querySelector('.hex-code')
 
 
 cngBtn.addEventListener('click', () => {
     const color = decimalColorCode()
-    document.body.style.backgroundColor = colorRGB(color)
+    colorDisplay.style.backgroundColor = colorRGB(color)
     rgbCode.innerHTML = colorRGB(color)
     hexCode.innerHTML=colorHEX(color)
 })
